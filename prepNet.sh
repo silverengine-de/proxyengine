e8cf29c4225766e9f9989c7d11da94e4c2eac43d
rainer <rainer@helios.fritz.box>
2018-10-10 14:44:12 +0200

    some first changes for DL380p Gen8

--- prepNet.sh
+++ prepNet.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-bricksDir=~/gitclones/NetBricks
-#sudo insmod ~/gitclones/NetBricks/3rdparty/dpdk/build/kmod/igb_uio.ko
+bricksDir=~/work/NetBricks
+sudo ip link set ens2f0 down
 sudo modprobe vfio_pci
 sudo insmod $bricksDir/3rdparty/dpdk/build/kmod/rte_kni.ko "kthread_mode=multiple"
 sudo $bricksDir/3rdparty/dpdk/usertools/dpdk-devbind.py --bind vfio-pci 07:00.0
 sudo $bricksDir/3rdparty/dpdk/usertools/dpdk-devbind.py --status
-sudo ip addr add 192.168.222.3/24 dev enp7s0f1
-sudo ip link set enp7s0f1 up
+sudo ip addr add 192.168.222.3/24 dev ens2f1
+sudo ip link set ens2f1 up
 
 
