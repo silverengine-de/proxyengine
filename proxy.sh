1080380e67de85009dd0d317d6bbdee8965da059
Rainer Stademann <rainer@spacebandit.de>
2018-10-29 20:26:00 +0100

    added unbind.sh

--- proxy.sh
+++ proxy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+#we flush the ARP cache because outdated ARP entries may let proxy fail
+sudo ip -s -s neigh flush all
 export RUST_BACKTRACE=1
 export RUST_LOG="tcp_proxy=debug,proxy_engine=debug,e2d2=debug"
-executable=`cargo build $1 --message-format=json | jq -r 'select((.profile.test == false) and (.target.name == "proxy_engine")) | .filenames[]'`
+executable=`cargo build $2 --message-format=json | jq -r 'select((.profile.test == false) and (.target.name == "proxy_engine")) | .filenames[]'`
 echo $executable
-sudo -E env "PATH=$PATH" $executable proxy_run.toml
+sudo -E env "PATH=$PATH" $executable $1
 
